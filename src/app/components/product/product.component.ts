a896e496ae7d30e99ce1435b349adc878736472b
TugcenurC <coskuntugcenur@gmail.com>
2021-03-22 01:15:18 +0300

    route added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import {HttpClient} from '@angular/common/http';
 import { ProductService } from 'src/app/services/product.service';
 @Component({
--- src/app/models/categoryResponseModel.ts
+++
@@ -1,6 +0,0 @@
-import { Category } from "./category";
-import { ResponseModel } from "./responseModel";
-
-export interface CategoryResponseMode extends ResponseModel{
-    data:Category[];
-}
